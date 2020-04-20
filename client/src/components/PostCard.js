4dd4d10a6d7fa07e2c18f7d203892136f2305541
unknown <mehedi5051@gmail.com>
2020-04-20 10:21:30 +0600

    delete button shows on users own post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Moment from "moment";
 import { Button, Card, Image, Icon, Label } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../context/auth";
+
 const PostCard = ({
 	post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) => {
+	const { user } = useContext(AuthContext);
 	const likePost = () => {
 		console.log("Like Post!!");
 	};
@@ -37,14 +40,24 @@ const PostCard = ({
 					</Label>
 				</Button>
 
-				<Button as="div" labelPosition="right" onClick={commentOnPost}>
+				<Button labelPosition="right" as={Link} to={`/post/${id}`}>
 					<Button color="blue" basic>
 						<Icon name="comments" />
 					</Button>
-					<Label as="a" basic color="blue" pointing="left">
+					<Label basic color="blue" pointing="left">
 						{commentCount}
 					</Label>
 				</Button>
+				{user && user.username === username && (
+					<Button
+						as="div"
+						color="red"
+						floated="right"
+						onClick={() => console.log("delete")}
+					>
+						<Icon name="trash" style={{ margin: 0 }} />
+					</Button>
+				)}
 			</Card.Content>
 		</Card>
 	);
