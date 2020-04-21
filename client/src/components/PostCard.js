d1eaeb81eb371b96803df1b9e5a82a30d163de1b
unknown <mehedi5051@gmail.com>
2020-04-21 11:46:39 +0600

    moved delete button to its own component and imported

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,14 +5,13 @@ import { Link } from "react-router-dom";
 
 import { AuthContext } from "../context/auth";
 import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
+
 const PostCard = ({
 	post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) => {
 	const { user } = useContext(AuthContext);
 
-	const commentOnPost = () => {
-		console.log("commentOnPost");
-	};
 	return (
 		<Card fluid>
 			<Card.Content>
@@ -38,16 +37,7 @@ const PostCard = ({
 						{commentCount}
 					</Label>
 				</Button>
-				{user && user.username === username && (
-					<Button
-						as="div"
-						color="red"
-						floated="right"
-						onClick={() => console.log("delete")}
-					>
-						<Icon name="trash" style={{ margin: 0 }} />
-					</Button>
-				)}
+				{user && user.username === username && <DeleteButton postId={id} />}
 			</Card.Content>
 		</Card>
 	);
