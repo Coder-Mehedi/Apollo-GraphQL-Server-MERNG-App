7f8bb258cda1f267d67bd6c4eb01ffae2a18e2d9
unknown <mehedi5051@gmail.com>
2020-04-22 11:12:09 +0600

    change some id type String to ID

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -49,9 +49,9 @@ const typeDefs = gql`
 		login(username: String!, password: String!): User!
 		createPost(body: String!): Post!
 		deletePost(postId: ID!): String!
-		createComment(postId: String!, body: String!): Post!
-		deleteComment(postId: String!, commentId: ID!): Post!
-		likePost(postId: String!): Post!
+		createComment(postId: ID!, body: String!): Post!
+		deleteComment(postId: ID!, commentId: ID!): Post!
+		likePost(postId: ID!): Post!
 	}
 	type Subscription {
 		newPost: Post!
