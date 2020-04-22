7e617cf227a3aa1ed998c355e0e798afb494ee66
unknown <mehedi5051@gmail.com>
2020-04-22 12:14:19 +0600

    change port and handle db connection err

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ const server = new ApolloServer({
 	context: ({ req }) => ({ req, pubsub }),
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
 	.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
 		console.log("MongoDB Database Connected");
-		return server.listen({ port: 5000 });
+		return server.listen({ port: PORT });
 	})
-	.then((res) => console.log(`server running at ${res.url}`));
+	.then((res) => console.log(`server running at ${res.url}`))
+	.catch((err) => console.error(err));
